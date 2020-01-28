376db4720004d351e35838988a604d4126c24d92
Jangjungho <wkdwjdgh1234@naver.com>
2020-01-28 12:18:53 +0900

    command 패턴 파일 오류 수정

---
+++ CPlusPlusDesignPattern/CommandPattern/CommandPattern.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../PatternTestInterface.h"
+class CommandPattern :public PatternBase
+{
+public:
+    CommandPattern(){}
+    virtual void DoPattern();
+};
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "CplusPlusGrammer/pointer/MyPointerModule.h"
+#include "CPlusPlusDesignPattern/CPlusPlusDesignPatternModuler.h"
+
+#include<iostream>
+
+int main()
+{
+    printf("main is started!!\n");
+    DoDesignPatternModule();
+    return 0;
+}
+
+void DoPointerModule()
+{
+    MyPointerModule mpm;
+    mpm.HowToUseFunctionoPointerTest1();
+}
+
+void DoDesignPatternModule()
+{
+    CPlusCPlusDesignPatternModuler DPM(CPlusCPlusDesignPatternModuler::ePatternType::commandPattern);
+    DPM.DoModule();
+    
+}
