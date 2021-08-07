c157453893a693db0c0d9e3b5708c001269a5ccf
Jangjungho <wkdwjdgh1234@naver.com>
2021-08-08 01:56:27 +0900

    파일정리

--- CPlusPlusMainProject/main.cpp
+++ CPlusPlusMainProject/main.cpp
@@ -1,46 +1,8 @@
 #include <iostream>
 using namespace std;
-
-#include "MyMonster.h"
-#include "MyHuman.h"
-
-/*
-목표
-1. 상속에 대한 개념을 이해한다.
-
-2. 클래스 포인터에 대한 개념을 이해합니다.
-    자식은 부모 클래스로 다뤄줄(묶을) 수 있다.
-
-3. 상속과 클래스 포인터의 응용 방법을 몇개 살펴보기
-
-4. 오버라이딩 overriding 기능의 응용
-              OverLoading 매개변수가 다른 함수.
-
-5. 클래스를 제대로 만드는 방법
-*/
-
 int main()
 {
-    MyMonster m1, m2, m3;
+	cout << "Hello World" << endl;
 
-    MyHuman h1, h2;
-
-    //사람이 몬스터를 공격한다!
-    h1.Attack(&m1);
-
-    //몬스터가 사람을 공격한다!
-    m1.Attack(&h1);
-
-    //사람과 몬스터의 hp를 출력
-    cout << "사람 hp" << endl;
-    h1.PrintHP();
-    h2.PrintHP();
-
-
-    cout << "몬스터 hp" << endl;
-    m1.PrintHP();
-    m2.PrintHP();
-    m3.PrintHP();
-
-    return 0;
+	return 0;
 }
--- README.md
+++ README.md
@@ -7,19 +7,23 @@ Description :
 c++문법과, 모듈등을 실험하는 프로젝트입니다.
 
 -List-
-1. c++ Grammer
+1. c++ Grammer  
     문법에 대한 프로젝트
-2. c++ Data Structure
+2. c++ Data Structure  
     자료구조에 대한 프로젝트
-3. ConcurrentProgramProject
+3. ConcurrentProgramProject  
     동시성 프로그램 프로젝트
-4. C++DesignPattern 
+4. C++DesignPattern   
     디자인 패턴에 대한 프로젝트
+5. CPlusPlusMainProject  
+    Test를 위한 프로젝트, 전체 프로젝트에 접근할 수 있습니다.
+6. MySTLProject  
+    다양한 일반 코드들이 들어가 있습니다.
 
 # VisualStudio Code accel
-단축키
-build   : ctrl + shift + B
-excute  : ctrl + alt + C 
+단축키  
+Build   : ctrl + shift + B  
+Excute  : ctrl + alt + C 
 
 
 # History
